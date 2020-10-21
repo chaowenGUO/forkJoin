5464aea435fc538b511cf0f1880c8f732a654f47
chaowen guo <chaowen.guo1@gmail.com>
2020-10-21 02:37:39 -0500

    Update Executor.java

--- Executor.java
+++ Executor.java
@@ -3,5 +3,6 @@ public class Executor
     public static void main(final String[] args)
     {
         final var executor = java.util.concurrent.Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        System.out.println(executor.submit(() -> return 1).get());
     }
 }    
