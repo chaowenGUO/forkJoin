bb0e48406cac650a8ca4f554d45ae70c66681340
chaowen guo <chaowen.guo1@gmail.com>
2020-10-22 02:23:41 -0500

    Update Executor.java

--- Executor.java
+++ Executor.java
@@ -5,13 +5,9 @@ public class Executor
         final var fixed = java.util.concurrent.Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
         System.out.println(fixed.submit(() -> 1).get());
 	fixed.shutdown();
-	/*final var scheduled = java.util.concurrent.Executors.newSingleThreadScheduledExecutor();
+	final var scheduled = (java.util.concurrent.ScheduledThreadPoolExecutor)java.util.concurrent.Executors.newSingleThreadScheduledExecutor();
 	scheduled.setContinueExistingPeriodicTasksAfterShutdownPolicy(true);
         scheduled.scheduleAtFixedRate(() -> System.out.println("fuck"), 0, 5, java.util.concurrent.TimeUnit.SECONDS);
-	scheduled.shutdown();*/
-	final var executor = new java.util.concurrent.ScheduledThreadPoolExecutor(1);
-        executor.setContinueExistingPeriodicTasksAfterShutdownPolicy(true);
-        executor.scheduleAtFixedRate(() -> System.out.println("Hello, World!"), 0L, 1L, java.util.concurrent.TimeUnit.SECONDS);
-        executor.shutdown();
+	scheduled.shutdown();
     }
 }    
